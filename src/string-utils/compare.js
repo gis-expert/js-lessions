741877447d4c70b4cdb236a6266126fe3e06b5c9
mer0sh <galmerkhat007@gmail.com>
2024-08-12 15:41:24 +0500

    Переделал файл compare.js

--- src/string-utils/compare.js
+++ src/string-utils/compare.js
@@ -4,15 +4,32 @@ function argumTypeMismatch(firstText, secondText) {
     if (typeof firstText !== 'string' || typeof secondText !== 'string') {
         throw Error('argument must be type of string');
     }
-
     return true;
+}
+
+function isMoreOrLess(firstText, secondText) {
+    const textLength = Math.max(len(firstText), len(secondText));
+    for (let a = 0; a !== textLength; a++) {
+        if (firstText.charCodeAt(a) > secondText.charCodeAt(a)) {
+            return true;
+        } 
+        if (firstText.charCodeAt(a) < secondText.charCodeAt(a)) {
+            return false;
+        }
+        if (isNaN(firstText.charCodeAt(a))) {
+                return false;
+        }
+        if (isNaN(secondText.charCodeAt(a))) {
+                return true;
+        }
+    }
 }
 
 /** возвращает булевый ответ равны ли параметры firstText и secondText. */
 export function isEqual(firstText, secondText) {
     argumTypeMismatch(firstText, secondText);
-
-    for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
+    const textLength = Math.max(len(firstText), len(secondText));
+    for (let a = 0; a !== textLength; a++) {
         if (firstText.charCodeAt(a) !== secondText.charCodeAt(a)) {
             return false;
         }
@@ -22,95 +39,41 @@ export function isEqual(firstText, secondText) {
 
 /** возвращает true если аргументы не равны, и true в иных случаях. */
 export function isNotEqual(firstText, secondText) {
-    argumTypeMismatch(firstText, secondText);
-
-    for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
-        if (firstText.charCodeAt(a) !== secondText.charCodeAt(a)) {
-            return true;
-        }
-    }
-    return false;
+    return !isEqual(firstText, secondText);
 }
 
 /** возвращает булевый ответ больше ли параметр firstText чем secondText. */
 export function isMore(firstText, secondText) {
     argumTypeMismatch(firstText, secondText);
-
-        for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
-            if (firstText.charCodeAt(a) > secondText.charCodeAt(a)) {
-                return true;
-            } 
-            if (firstText.charCodeAt(a) < secondText.charCodeAt(a)) {
-                return false;
-            }
-            if (isNaN(firstText.charCodeAt(a))) {
-                return false;
-            }
-            if (isNaN(secondText.charCodeAt(a))) {
-                return true;
-            }
-        }
-        return false;
+    if (isEqual(firstText, secondText)) {
+    return false;
+    }
+    return isMoreOrLess(firstText, secondText);
 }
 
 /** возвращает булевый ответ меньше ли параметр firstText чем secondText. */
 export function isLess(firstText, secondText) {
     argumTypeMismatch(firstText, secondText);
-
-    for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
-        if (firstText.charCodeAt(a) < secondText.charCodeAt(a)) {
-            return true;
-        } 
-        if (firstText.charCodeAt(a) > secondText.charCodeAt(a)) {
-            return false;
-        }
-        if (isNaN(firstText.charCodeAt(a))) {
-            return true;
-        }
-        if (isNaN(secondText.charCodeAt(a))) {
-            return false;
-        }
+    if (isEqual(firstText, secondText)) {
+        return false;
     }
-    return false;
-
+        return !isMoreOrLess(firstText, secondText);
 }
 
 /** возвращает булевый ответ больше или равно ли параметр firstText чем secondText. */
 export function isMoreOrEqual(firstText, secondText) {
     argumTypeMismatch(firstText, secondText);
-    for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
-        if (firstText.charCodeAt(a) < secondText.charCodeAt(a)) {
-            return false;
-        }
-        if (firstText.charCodeAt(a) > secondText.charCodeAt(a)) {
-            return true;
-        }
-        if (isNaN(firstText.charCodeAt(a))) {
-            return false;
-        }
-        if (isNaN(secondText.charCodeAt(a))) {
-            return true;
-        }
+    if (isEqual(firstText, secondText)) {
+        return true;
     }
-    return true;
+        return isMoreOrLess(firstText, secondText);
 }
 
 /** возвращает булевый ответ меньше или равно ли параметр firstText чем secondText. */
 export function isLessOrEqual(firstText, secondText) {
     argumTypeMismatch(firstText, secondText);
-    for (let a = 0; a !== Math.max(len(firstText), len(secondText)); a++) {
-        if (firstText.charCodeAt(a) < secondText.charCodeAt(a)) {
-            return true;
-        }
-        if (firstText.charCodeAt(a) > secondText.charCodeAt(a)) {
-            return false;
-        }
-        if (isNaN(firstText.charCodeAt(a))) {
-            return true;
-        }
-        if (isNaN(secondText.charCodeAt(a))) {
-            return false;
-        }
+    if (isEqual(firstText, secondText)) {
+        return true;
     }
-    return true;
+        return !isMoreOrLess(firstText, secondText);
 }
